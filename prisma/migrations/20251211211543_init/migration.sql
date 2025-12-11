ee16b4970e4c1a73e4ae640ba321c62b2e25b619
jvs-dev <shepherdcom12@gmail.com>
2025-12-11 18:18:03 -0300

    Restore SQLite configuration for local development

--- prisma/migrations/20251211211543_init/migration.sql
+++ prisma/migrations/20251211211543_init/migration.sql
@@ -4,6 +4,7 @@ CREATE TABLE "User" (
     "email" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "role" TEXT NOT NULL DEFAULT 'USER',
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL
 );
@@ -20,6 +21,20 @@ CREATE TABLE "Course" (
     "updatedAt" DATETIME NOT NULL,
     "userId" INTEGER NOT NULL,
     CONSTRAINT "Course_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Lesson" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "coverImage" TEXT,
+    "videoUrl" TEXT NOT NULL,
+    "status" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "courseId" INTEGER NOT NULL,
+    CONSTRAINT "Lesson_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "Course" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- CreateIndex
